96948b1f0d98766d0860309a8034f61066207822
Dashnyam <lord_dashka277@yahoo.com>
2020-11-23 19:19:56 +0800

    webpack tohiruulj zarim zohion baiguulalt hiiv mun admin panel html iig nemlee

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "admin.html",
+      template: "src/admin.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+};
